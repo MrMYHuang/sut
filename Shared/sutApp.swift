ebf96738dcd18e317507336ac99fa91307b71724
Meng-Yuan Huang <myh@live.com>
2022-09-11 11:56:29 +0800

    Initial Commit

---
+++ Shared/sutApp.swift
@@ -0,0 +1,17 @@
+//
+//  sutApp.swift
+//  Shared
+//
+//  Created by 黃孟遠 on 2022/9/11.
+//
+
+import SwiftUI
+
+@main
+struct sutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
